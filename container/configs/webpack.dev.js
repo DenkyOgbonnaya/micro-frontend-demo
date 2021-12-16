90a1a4c56971e18fd69e09073f5b60fcc7e463e9
DenkyOgbonnaya <denkyogb@gmail.com>
2021-12-16 10:00:43 +0100

    initial commit

---
+++ container/configs/webpack.dev.js
@@ -0,0 +1,29 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const devConfig = {
+  mode: "development",
+  output: {
+    publicPath: "http://localhost:8080/",
+  },
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ModuleFederationPlugin({
+      name: "container",
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
